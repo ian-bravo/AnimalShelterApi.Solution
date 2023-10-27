ef035f6e0a76d68fe98d6d256005bf8edbbb847a
ian-bravo <bravo.ian@gmail.com>
2023-10-27 10:05:28 -0700

    seed database

---
+++ AnimalShelterApi/Migrations/20231027170443_SeedData.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Age", "Name", "Sex", "Size" },
+                values: new object[,]
+                {
+                    { 1, 7, "Anya", "Female", "Large" },
+                    { 2, 10, "Faith", "Female", "Medium" },
+                    { 3, 2, "Willow", "Female", "Medium" },
+                    { 4, 4, "Spike", "Male", "Small" },
+                    { 5, 10, "Giles", "Male", "Medium" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Name", "Sex", "Size" },
+                values: new object[,]
+                {
+                    { 1, 5, "Beatrice", "Female", "Large" },
+                    { 2, 3, "Winky", "Male", "Small" },
+                    { 3, 2, "Hubert", "Male", "Large" },
+                    { 4, 4, "Rhapsody", "Female", "Large" },
+                    { 5, 7, "Jessica", "Female", "Medium" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 5);
+        }
+    }
+}
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -10,5 +10,26 @@ namespace AnimalShelterApi.Models
     public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Cat>()
+        .HasData(
+          new Cat { CatId = 1, Name = "Anya", Size = "Large", Sex = "Female", Age = 7 },
+          new Cat { CatId = 2, Name = "Faith", Size = "Medium", Sex = "Female", Age = 10 },
+          new Cat { CatId = 3, Name = "Willow", Size = "Medium", Sex = "Female", Age = 2 },
+          new Cat { CatId = 4, Name = "Spike", Size = "Small", Sex = "Male", Age = 4 },
+          new Cat { CatId = 5, Name = "Giles", Size = "Medium", Sex = "Male", Age = 10 }
+        );
+
+      builder.Entity<Dog>()
+        .HasData(
+          new Dog { DogId = 1, Name = "Beatrice", Size = "Large", Sex = "Female", Age = 5 },
+          new Dog { DogId = 2, Name = "Winky", Size = "Small", Sex = "Male", Age = 3 },
+          new Dog { DogId = 3, Name = "Hubert", Size = "Large", Sex = "Male", Age = 2 },
+          new Dog { DogId = 4, Name = "Rhapsody", Size = "Large", Sex = "Female", Age = 4 },
+          new Dog { DogId = 5, Name = "Jessica", Size = "Medium", Sex = "Female", Age = 7 }
+        );
+    }
   }
 }
