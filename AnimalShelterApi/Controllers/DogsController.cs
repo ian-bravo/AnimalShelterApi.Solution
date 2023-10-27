3dae4ce0d4ab8042a96f8ec44ccfebe41cbb169d
ian-bravo <bravo.ian@gmail.com>
2023-10-27 12:07:03 -0700

    create DogsController Edit/Put method

--- AnimalShelterApi/Controllers/DogsController.cs
+++ AnimalShelterApi/Controllers/DogsController.cs
@@ -41,5 +41,38 @@ namespace AnimalShelterApi.Controllers
       await _db.SaveChangesAsync();
       return CreatedAtAction(nameof(GetDog), new { id = dog.DogId }, dog);
     }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put (int id, Dog dog)
+    {
+      if (id != dog.DogId)
+      {
+        return BadRequest();
+      }
+
+      _db.Dogs.Update(dog);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!DogExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    private bool DogExists(int id)
+    {
+      return _db.Dogs.Any(e => e.DogId == id);
+    }
   }
 }
